652ca0e6059116b096911a9634d37f6970992352
Ronit Wilson <ronit.study@wsl>
2023-08-13 13:45:02 +0530

    get the debug route working

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.listen(port, ()=> {
+    console.log('Task Manager App log')
+})
+
+app.get("/debug", (req, res) => {
+    res.send("task manager app")
+})
